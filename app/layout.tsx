3c6c0762d3863872df0600ec32c25e8e8acbcbbf
Pablo Zallio <pablozalliodev@gmail.com>
2026-06-10 10:55:35 +0200

    [STYLE] - Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Cormorant_Garamond, Parisienne } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const cormorantGaramond = Cormorant_Garamond({
+      variable: "--font-cormorant-garamond",
+      subsets: ["latin"]
+})
+
+const parisienne = Parisienne({
+      variable: "--font-parisienne",
+      subsets: ["latin"],
+      weight: ["400"]
+})
+
+const inter = Inter({
+      variable: "--font-inter",
+      subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +32,18 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${cormorantGaramond.variable} ${inter.variable} ${parisienne.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
+
+
+/* 
+
+
+  --font-inter: var(--font-inter);
+  --font-cormorantGaramond: var(--font-cormorant-garamond);
+  
+  */
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
  <>
+ <h1 className=" font-inter ">Prueba</h1>
+ <h1 className=" font-cormorantGaramond italic text-6xl ">Los sabores que <span className=" font-parisienne ">hacen especiales</span> tus momentos</h1>
  </>
   );
 }
